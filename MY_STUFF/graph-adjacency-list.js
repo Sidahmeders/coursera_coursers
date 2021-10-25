e763c924a2a437f559b1c03cfbea2a1872608540
Sidahmeders <ahorob57@gmail.com>
2021-10-25 15:50:07 +0200

    added a parent Graph class to inherit in the directed and undircted graphs

--- MY_STUFF/graph-adjacency-list.js
+++ MY_STUFF/graph-adjacency-list.js
@@ -1,82 +1,89 @@
-
 class Graph {
-    #vertices = new Set()
-    #adjacentList = new Map()
-
-    get vertices() {
-        return this.#vertices
-    }
-
-    get edges() {
-        return this.#adjacentList
+    constructor () {
+        this.vertices = new Set()
+        this.adjacentList = new Map()
     }
 
     addVertex(vertex) {
-        if (vertex && !this.#vertices.has(vertex)) {
-            this.#vertices.add(vertex)
-            this.#adjacentList.set(vertex, new Set())
+        if (vertex && !this.vertices.has(vertex)) {
+            this.vertices.add(vertex)
+            this.adjacentList.set(vertex, new Set())
         }
-        return this.#vertices
+        return this.vertices
     }
 
     removeVertex(vertex) {
-        if (vertex && this.#vertices.has(vertex)) {
+        if (vertex && this.vertices.has(vertex)) {
             this.removeEdges(vertex)
-            this.#vertices.delete(vertex)
-            this.#adjacentList.delete(vertex)
+            this.vertices.delete(vertex)
+            this.adjacentList.delete(vertex)
         }
     }
 
-    bfs(vertex) {}
-
-    dfs(vertex) {}
-
-    addEdges(source, destination, undirected = false) {
-        if (source && destination) {
-            if (!this.#vertices.has(source)) this.addVertex(source)
-            if (!this.#vertices.has(destination)) this.addVertex(destination)
-            this.#adjacentList.get(source).add(destination)
-            if (undirected) this.#adjacentList.get(destination).add(source)
-        }
-    }
-
-    removeEdges(vertex) {
-        if (vertex) {
-            const verticesSet = this.#adjacentList.get(vertex)
-            if (verticesSet) {
-                verticesSet.forEach(v => {
-                    let set = this.#adjacentList.get(v)
-                    if (set) {
-                        set.delete(vertex)
-                    }
-                })
-                this.#adjacentList.set(vertex, new Set())
-            }
-        }
-    }
-
-    getGraphView() {
+    get graphView() {
         let graphView = ''
-        this.#adjacentList.forEach((val, key) => {
+        this.adjacentList.forEach((val, key) => {
             graphView += `\n ${key} -> ${Array.from(val).join(', ')}`
         })
         return graphView
     }
 }
 
+class DirectedGraph extends Graph {}
+
+class UndirectedGraph extends Graph {
+    addEdges(source, destination) {
+        if (source && destination) {
+            if (!this.vertices.has(source)) this.addVertex(source)
+            if (!this.vertices.has(destination)) this.addVertex(destination)
+            this.adjacentList.get(source).add(destination)
+            this.adjacentList.get(destination).add(source)
+        }
+    }
+
+    removeEdges(vertex) {
+        if (vertex) {
+            const verticesSet = this.adjacentList.get(vertex)
+            if (verticesSet) {
+                verticesSet.forEach(v => {
+                    let set = this.adjacentList.get(v)    
+                    set.delete(vertex)
+                })
+                this.adjacentList.set(vertex, new Set())
+            }
+        }
+    }
+
+    bfs(vertex) {
+        const queue = [vertex]
+        let neighbors
+        
+        while (queue.length) {
+            neighbors = this.adjacentList.get(vertex)
+            queue.push(...neighbors)
+        }
+
+        return queue
+    }
+
+    dfs(vertex) {}
+}
+
 // Set = [1]
 // Map = {1: Set(0)}
 
-const adjGraph = new Graph()
+const adjGraph = new UndirectedGraph()
 
 adjGraph.addVertex(1)
 adjGraph.addVertex(2)
 adjGraph.addEdges(2, 1)
 adjGraph.addEdges(2, 5)
+adjGraph.addEdges(5, 7)
 adjGraph.addEdges(7, 9)
-// adjGraph.removeVertex(1) // remove the vertex and all it's edges
-// adjGraph.removeEdges(2) // remove all the vertex edges
+// adjGraph.removeVertex(2) // remove the vertex and all it's edges
+// adjGraph.removeEdges(1) // remove all the vertex edges
 
 // console.log(adjGraph.getVertices())
 // console.log(adjGraph.getEdges())
-console.log(adjGraph.getGraphView())
+console.log(adjGraph.graphView)
+console.log(adjGraph.bfs(2))
